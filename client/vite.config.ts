147fec053ef36a57d9f0fc334f6a7eaa687a5494
Samuel Ramesh <sampingu02@gmail.com>
2023-12-01 08:58:19 +0530

    Add HTTPS support with required certificates

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,7 +1,16 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+import { readFileSync } from "node:fs";
+import { resolve } from "node:path";
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    https: {
+      key: readFileSync(resolve("private-key.pem")),
+      cert: readFileSync(resolve("certificate.crt")),
+    },
+  },
 });
